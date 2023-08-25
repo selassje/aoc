fe8c271a8956901d2d9dcbfafb285e1aab1dcbef
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-08-25 16:17:48 +0200

    try to make clang-format work on ubuntu

--- cmake/clang_format.cmake
+++ cmake/clang_format.cmake
@@ -3,6 +3,7 @@ file(GLOB_RECURSE
      *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI] *.hpp.in
      )
 
+
 list(FILTER ALL_CXX_SOURCE_FILES EXCLUDE REGEX ${CMAKE_BINARY_DIR})
 list(FILTER ALL_CXX_SOURCE_FILES EXCLUDE REGEX Catch2)
 
@@ -11,17 +12,19 @@ find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
   add_custom_target(
     clang-format
-    COMMAND clang-format
+    COMMAND ${CLANG_FORMAT}
     -i
     -style=file
     ${ALL_CXX_SOURCE_FILES}
     )
   add_custom_target(
     clang-format-check
-    COMMAND clang-format
+    COMMAND ${CLANG_FORMAT}
     -dry-run
     -style=file
     -Werror
     ${ALL_CXX_SOURCE_FILES}
     )
+else()
+  message("Clang-format not available")
 endif()

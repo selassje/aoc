d33c6a82238f96cdcb235fd1bc3296ab8190f16f
Przemyslaw  Koziol <przemkoz85@gmail.com>
2023-08-28 00:46:06 +0200

    formatting

--- src/day3.cpp
+++ src/day3.cpp
@@ -8,22 +8,27 @@ unsigned int
 solve(const std::vector<std::string>& input) noexcept
 {
   unsigned int result = 0;
+
+  auto get_priority = [](char c) {
+    const char base = static_cast<bool>(std::islower(c)) ? 'a' : 'A' - 26;
+    return 1 + c - base;
+  };
+
   for (const auto& rucksack : input) {
     assert(rucksack.size() % 2 == 0 && rucksack.size() >= 2);
 
-    const auto middle = [&rucksack](){
-        auto middle = rucksack.begin();
-        std::advance(middle, rucksack.size() / static_cast<std::size_t>(2));
-        return middle;
+    const auto middle = [&rucksack]() {
+      auto middle = rucksack.begin();
+      std::advance(middle, rucksack.size() / static_cast<std::size_t>(2));
+      return middle;
     }();
 
-    const auto common_type = std::find_first_of(
-      rucksack.begin(), middle, middle , rucksack.end());
+    const auto common_type =
+      std::find_first_of(rucksack.begin(), middle, middle, rucksack.end());
 
     assert(common_type != rucksack.end());
 
-    const unsigned int base = static_cast<bool>(std::islower(*common_type)) ? 'a' : 'A' - 26;
-    result += 1 + *common_type - base;
+    result += get_priority(*common_type);
   }
   return result;
 }

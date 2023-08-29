889b9db51529f9be7e2b0f9a9efd9aeaa1447cbc
Przemyslaw  Koziol <przemkoz85@gmail.com>
2023-08-29 23:33:57 +0200

    some compile flag setup

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -1,6 +1,21 @@
 
 if (MSVC)
-    add_compile_options(/W4)
+    set($COMPILE_FLAGS_TO_ADD /W4)
+    set($COMPILE_FLAGS_TO_REMOVE /W3)
 else()
-    add_compile_options(-Wall -Wextra -Wpedantic -Werror)
+    set($COMPILE_FLAGS_TO_ADD -Wall -Wextra -Wpedantic -Werror)
+    set($COMPILE_FLAGS_TO_REMOVE "")
 endif()
+
+macro(target_setup_compile_options TARGET)
+    separate_arguments(CXX_FLAGS NATIVE_COMMAND ${CMAKE_CXX_FLAGS})
+    foreach(FLAG ${$COMPILE_FLAGS_TO_REMOVE})
+       list(REMOVE_ITEM CXX_FLAGS ${FLAG})
+    endforeach()    
+    foreach(FLAG ${$COMPILE_FLAGS_TO_ADD})
+       list(APPEND CXX_FLAGS ${FLAG})
+    endforeach()    
+    set_target_properties(${TARGET} PROPERTIES COMPILE_OPTIONS "${CXX_FLAGS}")
+    message("Target " ${TARGET} " compile options : " ${CXX_FLAGS})
+endmacro()
+

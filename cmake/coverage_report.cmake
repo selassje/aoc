d3a14ec4195a1cfb9d42a7d6d5c9929a10554f75
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-09-02 23:11:40 +0200

    more coverage fixes

--- cmake/coverage_report.cmake
+++ cmake/coverage_report.cmake
@@ -1,13 +1,15 @@
 if (ENABLE_COVERAGE_REPORT)
-    
-message("Enabling Test Coverage Report")
+    message("Enabling Test Coverage Report")
 
-find_program(GCOV "gcov")
+    find_program(LCOV "lcov")
 
-    if(GCOV)
-
-    else()
-        message("GCOV not found. Test Coverage Report Disabled")
+    if(NOT LCOV)
+        message(SEND_ERROR "LCOV not found.Test Coverage Report Disabled")
     endif()
-
+  
+   add_custom_target(
+    generate-coverage-report
+    COMMAND ${LCOV} --capture --directory ${CMAKE_BINARY_DIR} --output ${CMAKE_BINARY_DIR}/coverage.info --exclude '*Catch2*' --exclude '*11/*'
+    COMMAND genhtml ${CMAKE_BINARY_DIR}/coverage.info --output ${CMAKE_BINARY_DIR}/lcov/index.html 
+    )
 endif()

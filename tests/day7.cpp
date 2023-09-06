830510ed979a4e997763631fe4d3839f170b639f
Przemyslaw  Koziol <przemkoz85@gmail.com>
2023-09-06 20:05:57 +0200

    parse input

--- tests/day7.cpp
+++ tests/day7.cpp
@@ -4,6 +4,7 @@
 
 #include <catch2/catch.hpp>
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -11,19 +12,56 @@
 
 namespace {
 
-    using aoc22::day7::Input;
+using aoc22::day7::Input;
+using aoc22::day7::Record;
 
-    Input readInput(std::string_view) {
-        Input input{};
-        return input;
+Input
+readInput(const std::string_view path)
+{
+  Input input{};
+
+  std::vector<std::string> words{};
+  auto split = [&words](auto& iss) {
+    words.clear();
+    std::copy(std::istream_iterator<std::string>(iss),
+              std::istream_iterator<std::string>(),
+              std::back_inserter(words));
+  };
+
+  std::ifstream ifs{ path.data() };
+  std::string line{};
+  const std::string command_prefix = "$";
+  const std::string ls_command = "ls";
+  const std::string cd_command = "cd";
+  const std::string directory_prefix = "dir";
+
+  while (std::getline(ifs, line)) {
+    Record record{};
+    std::istringstream iss{ line };
+    split(iss);
+    if (words[0] == command_prefix) {
+      if (words[1] == ls_command) {
+        record = aoc22::day7::List{};
+      } else if (words[1] == cd_command) {
+        record = aoc22::day7::ChangeDirectory{ .directory = words[2] };
+      } else {
+        std::abort();
+      }
+    } else if (words[0] == directory_prefix) {
+      record = aoc22::day7::Directory{ .name = words[1] };
+    } else {
+      record = aoc22::day7::File{.name = words[1], .size = std::stoull(words[0])};
     }
-    
-};
+    input.push_back(record);
+  }
+  return input;
+}
+
+}
 
 TEST_CASE("Day7 Example", "[Day7]")
 {
   const auto input = readInput(inputs::day7::EXAMPLE);
-  const auto& [part_1, part_2] =
-    aoc22::day7::solve(input);
+  const auto& [part_1, part_2] = aoc22::day7::solve(input);
   REQUIRE(part_1 == 95437);
 }

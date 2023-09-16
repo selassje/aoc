ab37bc7b04546fca51e2012a8fd73ca0406fb66d
Przemyslaw  Koziol <przemkoz85@gmail.com>
2023-09-17 01:52:14 +0200

    oopsie

--- tests/tests_main.hpp
+++ tests/tests_main.hpp
@@ -4,17 +4,24 @@
 
 #include <catch2/catch_session.hpp>
 #include <catch2/catch_test_macros.hpp>
+
+#ifdef ENABLE_FUZZ_TESTS
 #include <fuzztest/fuzztest.h>
 #include <fuzztest/init_fuzztest.h>
 #include <gtest/gtest.h>
+#endif
 
 int
 main(int argc, char* argv[])
 {
   const auto catchTestsResult = Catch::Session().run(argc, argv);
+ #ifdef ENABLE_FUZZ_TESTS
   testing::InitGoogleTest();
   fuzztest::InitFuzzTest(&argc, &argv);
   const auto fuzzTestResult = RUN_ALL_TESTS();
+  #else
+  const auto fuzzTestResult = 0;
+  #endif
   return fuzzTestResult + catchTestsResult;
 }
 

43ad70e9eaccc2bc17ffa2a94f634061d3fd4485
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-09-16 19:22:54 +0200

    more work

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -1,17 +1,19 @@
 
-if (MSVC)
-    add_compile_options(/W4 /WX /external:anglebrackets /external:W0 /permissive /wd4868 /wd5045 /wd4324)
-else()
+function(setup_compiler_flags TARGET)
 
-    add_compile_options(-Wall -Wextra -Wpedantic -Werror -Wshadow -Wnon-virtual-dtor -Wold-style-cast
-                        -Wcast-align -Wunused -Woverloaded-virtual -Wconversion -Wsign-conversion -Wmisleading-indentation 
-                        -Wnull-dereference  -Wdouble-promotion -Wformat=2 -Wimplicit-fallthrough)
+if (MSVC)
+     target_compile_options(${TARGET} PRIVATE /W4 /WX /external:anglebrackets /external:W0 /permissive /wd4868 /wd5045 /wd4324)
+else()
+     target_compile_options(${TARGET} PRIVATE -Wall -Wextra -Wpedantic -Werror -Wshadow -Wnon-virtual-dtor -Wold-style-cast
+                            -Wcast-align -Wunused -Woverloaded-virtual -Wconversion -Wsign-conversion -Wmisleading-indentation 
+                            -Wnull-dereference  -Wdouble-promotion -Wformat=2 -Wimplicit-fallthrough)
                         
     if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" AND NOT ${ENABLE_CLANG_TIDY})
-        add_compile_options(-Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wuseless-cast)
+        target_compile_options(${TARGET} PRIVATE -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wuseless-cast)
     endif()
 
     if (ENABLE_COVERAGE_REPORT)
-        add_compile_options(--coverage -fno-elide-constructors)
+        target_compile_options( PRIVATE --coverage -fno-elide-constructors)
     endif()
 endif()
+endfunction(setup_compiler_flags TARGET)

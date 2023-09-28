87d65082d22415d5326bd4c644e8dd4ed7b8a5de
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-09-28 15:00:28 +0200

    more fiddling

--- cmake/setup_modules.cmake
+++ cmake/setup_modules.cmake
@@ -7,10 +7,13 @@ if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
   string(CONCAT CMAKE_EXPERIMENTAL_CXX_SCANDEP_SOURCE
   "<CMAKE_CXX_COMPILER> <DEFINES> <INCLUDES> <FLAGS> <SOURCE>"
   " -MT <DYNDEP_FILE> -MD -MF <DEP_FILE>"
-  " ${flags_to_scan_deps} -fdep-file=<DYNDEP_FILE> -fdep-output=<OBJECT>"
+  " ${flags_to_scan_deps} -fdeps-file=<DYNDEP_FILE> -fdeps-target=<OBJECT>"
   )
 
   set(CMAKE_EXPERIMENTAL_CXX_MODULE_MAP_FORMAT "gcc")
   set(CMAKE_EXPERIMENTAL_CXX_MODULE_MAP_FLAG
   "${compiler_flags_for_module_map} -fmodule-mapper=<MODULE_MAP_FILE>")
+
+  #add_compile_options(-fmodules-ts)
+
 endif()

ccdf28edcdc77361e4915949616185d0dfcea0f9
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-09-29 11:56:54 +0200

    input parsing

--- tests/day11.cpp
+++ tests/day11.cpp
@@ -9,6 +9,7 @@
 #include <sstream>
 
 using aoc22::day11::Input;
+using aoc22::day11::Monkey;
 
 Input
 readInput(const std::string_view path)
@@ -25,20 +26,52 @@ readInput(const std::string_view path)
 
   std::ifstream ifs{ path.data() };
   std::string line{};
-  const std::string noopIntruction = "noop";
-  const std::string addXInstruction = "addx";
 
+  auto toULL =
+    [end = static_cast<char*>(nullptr)](const std::string& word) mutable {
+      return std::strtoull(word.c_str(), &end, 10);
+    };
+
+  Monkey monkey{};
   while (std::getline(ifs, line)) {
-  //  Instruction instruction{};
     std::istringstream iss{ line };
     split(iss);
-    if (words[0] == addXInstruction) {
-   //   instruction = aoc22::day10::AddX{ std::stoll(words[1]) };
-    } else if (words[0] == noopIntruction) {
-     // instruction = aoc22::day10::Noop{};
+
+    if (line == "") {
+      input.push_back(monkey);
+      monkey.worryLevels.clear();
     }
-    //input.push_back(instruction);
+
+    if (words[0] == "Starting") {
+      for (std::size_t i = 2; i < words.size(); ++i) {
+        monkey.worryLevels.push_back(toULL(words[i]));
+      }
+
+    } else if (words[0] == "Operation:") {
+      auto getOperand =
+        [&toULL](const std::string& word) -> aoc22::day11::Operand {
+        if (word == "old") {
+          return aoc22::day11::Old{};
+        }
+        return aoc22::day11::Literal{ toULL(word) };
+      };
+
+      monkey.operation.op1 = getOperand(words[3]);
+      monkey.operation.op2 = getOperand(words[5]);
+
+      monkey.operation.type = words[4] == "+"
+                                ? aoc22::day11::OperationType::Add
+                                : aoc22::day11::OperationType::Multiply;
+
+    } else if (words[0] == "Test:") {
+      monkey.divisionTest = toULL(words.back());
+    } else if (words[0] == "If" && words[1] == "true:") {
+      monkey.nextMonkeyTestPass = toULL(words.back());
+    } else if (words[0] == "If" && words[1] == "false:") {
+      monkey.nextMonkeyTestFail = toULL(words.back());
+    }
   }
+  input.push_back(monkey);
   return input;
 }
 

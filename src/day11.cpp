34a898150a39c4aced26dc353b0f7c4626f71beb
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-09-29 16:01:46 +0200

    part2

--- src/day11.cpp
+++ src/day11.cpp
@@ -5,12 +5,14 @@
 
 namespace aoc22::day11 {
 
-Result
-solve(const Input& input)
+template<int ROUNDS, int DIVIDE_NEW_LEVEL>
+std::uint64_t solveInternal(const Input& input)
 {
   std::vector<std::vector<std::uint64_t>> worryLevels{};
+  std::uint64_t MODULO = 1;
   for (const auto& m : input) {
     worryLevels.push_back(m.worryLevels);
+    MODULO *= m.divisionTest;
   }
 
   auto getOperandValue = [](const std::uint64_t worryLevel,
@@ -22,27 +24,29 @@ solve(const Input& input)
   };
 
   auto getNewLevel = [&](const std::uint64_t worryLevel,
+                        // const std::uint64_t modulo,
                          const Operation& operation) {
-    const auto operandValue1 = getOperandValue(worryLevel, operation.op1);
-    const auto operandValue2 = getOperandValue(worryLevel, operation.op2);
+    const auto operandValue1 = getOperandValue(worryLevel, operation.op1) % MODULO;
+    const auto operandValue2 = getOperandValue(worryLevel, operation.op2) % MODULO;
 
     if (operation.type == OperationType::Add) {
-      return operandValue1 + operandValue2;
+      return (operandValue1 + operandValue2) % MODULO;
     }
-    return operandValue1 * operandValue2;
+    return (operandValue1 * operandValue2) % MODULO;
   };
 
   std::vector<std::size_t> inspectionsCount(input.size(), 0);
 
-  for (int round = 0; round < 20; ++round) {
+  for (int round = 0; round < ROUNDS; ++round) {
     for (std::size_t i = 0; i < input.size(); ++i) {
       const auto& monkey = input[i];
       for (const auto worryLevel : worryLevels[i]) {
-        const auto newLevel = getNewLevel(worryLevel, monkey.operation) / 3;
+        const auto newLevel =
+          getNewLevel(worryLevel,  monkey.operation) / DIVIDE_NEW_LEVEL;
         const auto nextMonkey = newLevel % monkey.divisionTest == 0
                                   ? monkey.nextMonkeyTestPass
                                   : monkey.nextMonkeyTestFail;
-  
+
         worryLevels[nextMonkey].push_back(newLevel);
       }
       inspectionsCount[i] += worryLevels[i].size();
@@ -52,8 +56,13 @@ solve(const Input& input)
 
   std::ranges::sort(inspectionsCount, std::ranges::greater{});
 
-  const auto resultPart1 = inspectionsCount[0] * inspectionsCount[1];
+  return inspectionsCount[0] * inspectionsCount[1];
+}
 
-  return std::make_pair(resultPart1, resultPart1);
+Result
+solve(const Input& input)
+{
+  return std::make_pair(solveInternal<20, 3>(input),
+                        solveInternal<10000, 1>(input));
 }
 };
--- tests/day11.cpp
+++ tests/day11.cpp
@@ -78,7 +78,7 @@ TEST_CASE("Day11 Example", "[Day11]")
   const auto input = readInput(inputs::day11::EXAMPLE);
   const auto& [part_1, part_2] = aoc22::day11::solve(input);
   REQUIRE(part_1 == 10605);
-  REQUIRE(part_2 == 10605);
+  REQUIRE(part_2 == 2713310158);
 }
 
 TEST_CASE("Day11 Input", "[Day11]")
@@ -86,5 +86,5 @@ TEST_CASE("Day11 Input", "[Day11]")
   const auto input = readInput(inputs::day11::INPUT);
   const auto& [part_1, part_2] = aoc22::day11::solve(input);
   REQUIRE(part_1 == 61005);
-  REQUIRE(part_2 == 61005);
+  REQUIRE(part_2 == 20567144694);
 }

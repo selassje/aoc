be23b5bd815e96a15fd6b8e81fc081d96da1df15
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-10-04 00:40:51 +0200

    mehr

--- tests/day11.cpp
+++ tests/day11.cpp
@@ -96,8 +96,8 @@ TEST_CASE("Day11 Input", "[Day11]")
 }
 
 #ifdef ENABLE_FUZZ_TESTS
-#include <fuzztest/fuzztest.h>
 #include <cstdint>
+#include <fuzztest/fuzztest.h>
 
 using aoc22::day11::Operand;
 using aoc22::day11::Operation;

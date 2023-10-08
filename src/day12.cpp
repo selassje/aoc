74337082e7771c2594f6b48a790b68e74f2e93a1
Przemyslaw  Koziol <przemkoz85@gmail.com>
2023-10-08 19:30:30 +0200

    more

--- src/day12.cpp
+++ src/day12.cpp
@@ -1,10 +1,10 @@
 #include "day12.hpp"
 
-
-#include <queue>
 #include <limits>
+#include <queue>
 #include <unordered_map>
 #include <unordered_set>
+
 
 template<>
 struct std::hash<aoc22::day12::Position>
@@ -24,7 +24,7 @@ solve(const Input& input)
   const auto& grid = input.grid;
   const auto height = grid.size();
   const auto width = grid[0].size();
-  const auto maxDistance =  std::numeric_limits<decltype(Result::Part1)>::max(); 
+  const auto maxDistance = std::numeric_limits<decltype(Result::Part1)>::max();
 
   std::unordered_set<Position> visitedPositions{};
   std::queue<Position> toBeVisitedPositions{};
@@ -32,22 +32,50 @@ solve(const Input& input)
 
   for (IndexType y = 0; y < height; ++y) {
     for (IndexType x = 0; x < width; ++x) {
-      const auto position = Position{x,y};
-      auto &distance = distances[position]; 
+      const auto position = Position{ x, y };
+      auto& distance = distances[position];
       if (input.startPosition != position) {
-        distance = maxDistance; 
+        distance = maxDistance;
       }
     }
   }
 
+  auto getNeighbours = [&](const auto& position) {
+    std::vector<Position> neighbours{};
+    const auto& [x, y] = position;
+    if (x > 0) {
+      neighbours.emplace_back(Position{ x - 1, y });
+    }
+    if (x < width - 1) {
+      neighbours.emplace_back(Position{ x + 1, y });
+    }
+    if (y > 0) {
+      neighbours.emplace_back(Position{ x, y - 1 });
+    }
+    if (y < height - 1) {
+      neighbours.emplace_back(Position{ x, y + 1 });
+    }
+    return neighbours;
+  };
+
+  auto isStepPossible = [&](const auto& src, const auto& dst) {
+    return grid[src.y][src.x] + 1 >= grid[dst.y][dst.x];
+  };
+
   toBeVisitedPositions.push(input.startPosition);
   while (!toBeVisitedPositions.empty()) {
+    const auto position = toBeVisitedPositions.front();
+    for (const auto& neighbour : getNeighbours(position)) {
+      if (isStepPossible(position, neighbour) &&
+          visitedPositions.contains(neighbour)) {
+      }
+    }
 
-    visitedPositions.insert(toBeVisitedPositions.front());
+    visitedPositions.insert(position);
     toBeVisitedPositions.pop();
   }
 
-  return { input.grid.size(), input.grid.size() };
+  return { distances[input.finalPosition], input.grid.size() };
 }
 
 };

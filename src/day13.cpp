2f907ac9b7ef76594c5e9e82777a2a372cb5d5bd
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-10-11 05:22:57 +0200

    cleaning

--- src/day13.cpp
+++ src/day13.cpp
@@ -12,43 +12,42 @@ namespace aoc22::day13 {
 
 struct List;
 
-using Element = std::variant<std::shared_ptr<List>, std::size_t>;
+using Packet = std::variant<std::shared_ptr<List>, std::size_t>;
 
 struct List
 {
-  std::vector<Element> elements;
+  std::vector<Packet> packets;
 };
 
-Element
-parse(std::string_view tokens)
+Packet
+parse(std::string_view packet)
 {
-  if (tokens[0] == '[') {
+  if (packet[0] == '[') {
     auto listPtr = std::make_shared<List>();
-
-    std::size_t nextElementStart = 0;
+    std::size_t nextSubpacketStart = 0;
     std::size_t level = 0;
 
-    for (std::size_t i = 1; i < tokens.size(); ++i) {
-      if ((tokens[i] == ',' || i == tokens.size() - 1) && level == 0) {
-        auto elementStr =
-          tokens.substr(nextElementStart + 1, i - nextElementStart - 1);
-        if (!elementStr.empty()) {
-          listPtr->elements.push_back(parse(elementStr));
+    for (std::size_t i = 1; i < packet.size(); ++i) {
+      if ((packet[i] == ',' || i == packet.size() - 1) && level == 0) {
+        if (const auto subPacket =
+              packet.substr(nextSubpacketStart + 1, i - nextSubpacketStart - 1);
+            !subPacket.empty()) {
+          listPtr->packets.push_back(parse(subPacket));
         }
-        nextElementStart = i;
-      } else if (tokens[i] == '[') {
+        nextSubpacketStart = i;
+      } else if (packet[i] == '[') {
         ++level;
-      } else if (tokens[i] == ']') {
+      } else if (packet[i] == ']') {
         --level;
       }
     }
     return listPtr;
   } else {
     const auto integerEnd = std::ranges::find_if(
-      tokens, [](char token) { return token == ',' || token == ']'; });
-    std::string strInteger{ tokens.begin(), integerEnd };
+      packet, [](char token) { return token == ',' || token == ']'; });
+    std::string integer{ packet.begin(), integerEnd };
     return static_cast<std::size_t>(
-      std::strtoull(strInteger.c_str(), nullptr, 10));
+      std::strtoull(integer.c_str(), nullptr, 10));
   }
 }
 
@@ -62,7 +61,7 @@ enum class CmprResult
 using enum CmprResult;
 
 CmprResult
-compare(const Element& left, const Element& right)
+compare(const Packet& left, const Packet& right)
 {
   CmprResult result = Equal;
   if (left.index() == 1 && right.index() == 1) {
@@ -77,29 +76,29 @@ compare(const Element& left, const Element& right)
     const auto& leftList = std::get<0>(left);
     const auto& rightList = std::get<0>(right);
     const auto minSize =
-      std::min(leftList->elements.size(), rightList->elements.size());
+      std::min(leftList->packets.size(), rightList->packets.size());
     for (std::size_t i = 0; i < minSize; ++i) {
       const auto cmprResult =
-        compare(leftList->elements[i], rightList->elements[i]);
+        compare(leftList->packets[i], rightList->packets[i]);
       if (cmprResult != Equal) {
         result = cmprResult;
         break;
       }
     }
     if (result == Equal) {
-      if (leftList->elements.size() < rightList->elements.size()) {
+      if (leftList->packets.size() < rightList->packets.size()) {
         result = Lesser;
-      } else if (leftList->elements.size() > rightList->elements.size()) {
+      } else if (leftList->packets.size() > rightList->packets.size()) {
         result = Greater;
       }
     }
   } else if (left.index() == 1) {
     auto listPtr = std::make_shared<List>();
-    listPtr->elements.emplace_back(std::get<1>(left));
+    listPtr->packets.emplace_back(std::get<1>(left));
     result = compare(std::move(listPtr), right);
   } else {
     auto listPtr = std::make_shared<List>();
-    listPtr->elements.emplace_back(std::get<1>(right));
+    listPtr->packets.emplace_back(std::get<1>(right));
     result = compare(left, std::move(listPtr));
   }
   return result;
@@ -128,26 +127,18 @@ solve(const Input& input)
     return compare(left, right) == Lesser;
   });
 
-  auto divider1Index_ =
-    std::ranges::find_if(elements,
-                         [&divider1](const auto& packet) {
-                           return compare(packet, divider1) == Equal;
-                         }) -
-    elements.begin();
+  auto findIndex = [&elements](const auto& packet) {
+    const auto index =
+      std::ranges::find_if(elements, [&packet](const auto& packet_) {
+        return compare(packet, packet_) == Equal;
+      });
+    return static_cast<std::size_t>(index - elements.begin()) + 1;
+  };
 
-  auto divider2Index_ =
-    std::ranges::find_if(elements,
-                         [&divider2](const auto& packet) {
-                           return compare(packet, divider2) == Equal;
-                         }) -
-    elements.begin();
+  const auto divider1Index = findIndex(divider1);
+  const auto divider2Index = findIndex(divider2);
 
-  const auto divider1Index = static_cast<std::size_t>(divider1Index_);
-  const auto divider2Index = static_cast<std::size_t>(divider2Index_);
-
-  const auto resultPart2 = (divider1Index + 1) * (divider2Index + 1);
-
-  return { resultPart1, resultPart2 };
+  return { resultPart1, divider1Index * divider2Index };
 }
 
 }

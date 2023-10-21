4a6400e29f0d95e7370c7b003ce5df99ea9636a4
Przemyslaw  Koziol <przemkoz85@gmail.com>
2023-10-21 12:44:30 +0200

    brute force prep

--- src/day16.cpp
+++ src/day16.cpp
@@ -1,9 +1,23 @@
 #include "day16.hpp"
 
+#include <algorithm>
+#include <ranges>
+#include <string>
+
 namespace aoc22::day16 {
 
+Valve getValve(std::string_view name, const Input& input) {
+  return *std::ranges::find(input, name, &Valve::name);     
+}
+
+std::size_t maxPressureRelease(std::string valveName, std::size_t minutes, Input input ) {
+  return 0;
+}
+
+
 Result solve(const Input &input) {
-    return {input.size(), input.size()};
+    const auto part1 = maxPressureRelease("AA",30, input);
+    return {part1, part1};
 }
 
 };

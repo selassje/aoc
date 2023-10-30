2840a67876964335cf6652bd8ed5207c5460ece4
Przemyslaw  Koziol <przemkoz85@gmail.com>
2023-10-30 23:29:39 +0100

    more

--- src/day17.cpp
+++ src/day17.cpp
@@ -1,8 +1,75 @@
 #include "day17.hpp"
 
+#include <cstddef>
+#include <variant>
+
 namespace aoc22::day17 {
-  Result solve(const Input&) {
-    return {0,0};
+
+struct Point
+{
+  std::size_t x;
+  std::size_t y;
+};
+
+struct RockCommon
+{
+  Point bottomLeft;
+};
+
+struct HorizontalLine : RockCommon
+{};
+struct Cross : RockCommon
+{};
+struct ReversedL : RockCommon
+{};
+struct VerticalLine : RockCommon
+{};
+struct Square : RockCommon
+{};
+
+using Rock =
+  std::variant<HorizontalLine, Cross, ReversedL, VerticalLine, Square>;
+
+auto getNextRock = [i = std::size_t{0}](const Point& leftBottomEdge) mutable -> Rock {
+  switch(i) {
+    case 0:
+      return HorizontalLine{leftBottomEdge};
+    case 1:
+      return Cross{leftBottomEdge};
+    case 2:
+      return ReversedL{leftBottomEdge};
+    case 3:
+      return VerticalLine{leftBottomEdge};
+    case 4:
+      return Square{leftBottomEdge};
+    default:
+      std::abort();
   }
+  i =  (i + 1) % std::variant_size_v<Rock>;
+};
+
+
+Result
+solve(const Input& input)
+{
+  static constexpr std::size_t chamberWidth = 7;
+  static constexpr std::size_t targetStoppedRocksCount = 2022;
+  const std::size_t jetCount = input.size();
+
+  std::size_t heighestRock = 0;
+  std::vector<Rock> stoppedRocks{};
+  static constexpr std::size_t leftEdge = 2;
+  while ( stoppedRocks.size() < targetStoppedRocksCount) {
+    auto rock = getNextRock({leftEdge, heighestRock + 4});
+    std::size_t i = 0;
+    while(true) {
+      const auto jet = input[i];
+      i = (i + 1) % jetCount;
+    }
+    stoppedRocks.emplace_back(rock);
+  }
+
+  return { heighestRock, heighestRock };
+}
 
 }

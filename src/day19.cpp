e950f22c52f5c80f2919f94ac45d3674d22edc09
Przemyslaw  Koziol <przemkoz85@gmail.com>
2023-11-04 18:30:39 +0100

    more

--- src/day19.cpp
+++ src/day19.cpp
@@ -1,7 +1,17 @@
 #include "day19.hpp"
 
 namespace aoc22::day19 {
-  Result solve(const Input&) {
-      return {33,33};
+
+  auto findMaxGeodes(const Blueprint& blueprint)  {
+      auto maxGeodes = blueprint.clayRobotCost.ore;
+      return maxGeodes;
+  };
+
+  Result solve(const Input& input) {
+    std::size_t part1 = 0;
+    for ( const auto& bluepint : input) {
+      part1 += static_cast<std::size_t>(bluepint.id * findMaxGeodes(bluepint));
+    }
+    return {part1,part1};
   }
 }

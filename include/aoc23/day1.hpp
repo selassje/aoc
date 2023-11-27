5c9caed62d7927224b7000dfafa09e60484cc615
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-11-28 00:06:41 +0100

    init

---
+++ include/aoc23/day1.hpp
@@ -0,0 +1,17 @@
+#ifndef AOC23_DAY1_HPP
+#define AOC23_DAY1_HPP
+
+#include <cstddef>
+
+namespace aoc23::day1 {
+
+struct Input {
+};
+
+struct Result {
+  std::size_t part1;
+  std::size_t part2;
+};
+
+}
+#endif

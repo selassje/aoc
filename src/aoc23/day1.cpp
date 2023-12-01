14b0a2a16ac6c0a8d003ef024dcf44a4fcac408f
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-12-01 08:29:34 +0100

    some refactoring

--- src/aoc23/day1.cpp
+++ src/aoc23/day1.cpp
@@ -9,50 +9,29 @@
 
 namespace aoc23::day1 {
 
-const std::map<std::string, std::size_t> strToDigitsPart1 = {
-  { "1", 1 }, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 },
-  { "6", 6 }, { "7", 7 }, { "8", 8 }, { "9", 9 }
-};
+using DigitMap = std::map<std::string_view, std::size_t>;
 
-const std::map<std::string, std::size_t> strToDigitsPart2 = {
+const DigitMap digitMapPart1 = { { "1", 1 }, { "2", 2 }, { "3", 3 },
+                                 { "4", 4 }, { "5", 5 }, { "6", 6 },
+                                 { "7", 7 }, { "8", 8 }, { "9", 9 } };
+
+const DigitMap digitMapPart2 = {
   { "1", 1 },     { "2", 2 },     { "3", 3 },    { "4", 4 },    { "5", 5 },
   { "6", 6 },     { "7", 7 },     { "8", 8 },    { "9", 9 },    { "one", 1 },
   { "two", 2 },   { "three", 3 }, { "four", 4 }, { "five", 5 }, { "six", 6 },
   { "seven", 7 }, { "eight", 8 }, { "nine", 9 }
 };
 
-Result
-solve(const Input& input)
+std::size_t
+solve_internal(const Input& input, const DigitMap& digitMap)
 {
-  std::size_t part1 = 0;
-  std::size_t part2 = 0;
-  for (const auto& line : input) {
-    std::size_t minIndexFirst = std::numeric_limits<std::size_t>::max();
-    std::size_t maxIndexLast = std::numeric_limits<std::size_t>::min();
-    std::size_t firstDigit = 0;
-    std::size_t lastDigit = 0;
-    for (const auto& [str, digit] : strToDigitsPart1) {
-      const auto firstDigitIndex = line.find_first_of(str);
-      if (firstDigitIndex != std::string::npos &&
-          firstDigitIndex < minIndexFirst) {
-        minIndexFirst = firstDigitIndex;
-        firstDigit = digit;
-      }
-      const auto lastDigitIndex = line.find_last_of(str);
-      if (lastDigitIndex != std::string::npos &&
-          lastDigitIndex >= maxIndexLast) {
-        maxIndexLast = lastDigitIndex;
-        lastDigit = digit;
-      }
-    }
-    part1 += firstDigit * 10 + lastDigit;
-  }
+  std::size_t result = 0;
   for (const auto& line : input) {
     std::size_t minIndexFirst = std::numeric_limits<std::size_t>::max();
     std::size_t maxIndexLast = std::numeric_limits<std::size_t>::max();
     std::size_t firstDigit = 0;
     std::size_t lastDigit = 0;
-    for (const auto& [str, digit] : strToDigitsPart2) {
+    for (const auto& [str, digit] : digitMap) {
       const auto firstDigitIndex = line.find(str);
       if (firstDigitIndex != std::string::npos &&
           firstDigitIndex < minIndexFirst) {
@@ -68,8 +47,15 @@ solve(const Input& input)
         lastDigit = digit;
       }
     }
-    part2 += firstDigit * 10 + lastDigit;
+    result += firstDigit * 10 + lastDigit;
   }
-  return { part1, part2 };
+  return result;
+}
+
+Result
+solve(const Input& input)
+{
+  return { solve_internal(input, digitMapPart1),
+           solve_internal(input, digitMapPart2) };
 }
 }

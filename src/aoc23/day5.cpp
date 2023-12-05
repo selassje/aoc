eb0f7fe936f0155fdcc51c8d5348404d4e5dbccf
Przemyslaw Koziol <przemkoz85@gmail.com>
2023-12-05 15:18:16 +0100

    more cleaning

--- src/aoc23/day5.cpp
+++ src/aoc23/day5.cpp
@@ -3,19 +3,20 @@
 #include <array>
 #include <cstddef>
 #include <limits>
+#include <optional>
 
 namespace aoc23::day5 {
 
-struct Destination
+struct DestinationAndRemainingLengthInTheCurrentRange
 {
   std::size_t destination;
-  std::size_t remainingRange;
+  std::size_t remainingLength;
 };
 
 using MapArray = std::array<Map, 7>;
 
-Destination
-getDestination(std::size_t source, const Map& map)
+DestinationAndRemainingLengthInTheCurrentRange
+getDestinationAndRemainingLength(std::size_t source, const Map& map)
 {
   auto remainingRange = std::numeric_limits<std::size_t>::max();
   bool nextRangeFound = false;
@@ -32,29 +33,6 @@ getDestination(std::size_t source, const Map& map)
   return { source, remainingRange };
 };
 
-Destination
-getSeedLocation(std::size_t seed, const Input& input)
-{
-  const auto [soil, soilRange] = getDestination(seed, input.seedToSoil);
-  const auto [fertilizer, fertilizerRange] =
-    getDestination(soil, input.soilToFertilizer);
-  const auto [water, waterRange] =
-    getDestination(fertilizer, input.fertilizerToWater);
-  const auto [light, lightRange] = getDestination(water, input.waterToLight);
-  const auto [temp, tempRange] = getDestination(light, input.lightToTemp);
-  const auto [humidity, humidityRange] =
-    getDestination(temp, input.tempToHumidity);
-  const auto [location, locationRange] =
-    getDestination(humidity, input.humidityToLocation);
-  const auto minRange = std::min({ soilRange,
-                                   fertilizerRange,
-                                   waterRange,
-                                   lightRange,
-                                   tempRange,
-                                   humidityRange,
-                                   locationRange });
-  return { location, minRange };
-}
 
 Result
 solve(const Input& input)
@@ -63,14 +41,19 @@ solve(const Input& input)
                        input.fertilizerToWater,   input.waterToLight,
                        input.lightToTemp,         input.tempToHumidity,
                        input.humidityToLocation };
+  
+  struct LocationAndMinLength {
+    std::size_t location;
+    std::size_t minLength;
+  };
 
-  auto getLocationAndMinLength = [&maps](const std::size_t seed ) -> Destination {
+  auto getLocationAndMinLength = [&maps](const std::size_t seed ) -> LocationAndMinLength {
     std::size_t minLength = std::numeric_limits<std::size_t>::max();
     std::size_t location = seed;
     for ( const auto& map : maps) {
-        const auto destination = getDestination(location,map);
+        const auto destination = getDestinationAndRemainingLength(location,map);
         location = destination.destination;
-        minLength = std::min(minLength,destination.remainingRange);
+        minLength = std::min(minLength,destination.remainingLength);
     }
     return {location, minLength};
   };
@@ -79,14 +62,14 @@ solve(const Input& input)
   std::size_t part2 = std::numeric_limits<std::size_t>::max();
   for (std::size_t i = 0; i < input.seeds.size(); ++i) {
     auto seed = input.seeds[i];
-    part1 = std::min(part1, getLocationAndMinLength(seed).destination);
+    part1 = std::min(part1, getLocationAndMinLength(seed).location);
     if ((i + 1) % 2 == 0) {
       seed = input.seeds[i - 1];
       const auto endSeed = seed + input.seeds[i] - 1;
       while (seed <= endSeed) {
         auto location = getLocationAndMinLength(seed);
-        part2 = std::min(part2, location.destination);
-        seed += location.remainingRange + 1;
+        part2 = std::min(part2, location.location);
+        seed += location.minLength + 1;
       }
     }
   }

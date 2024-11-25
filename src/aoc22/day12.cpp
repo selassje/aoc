11c5fa344c5116580c4d4c1d572b322daece0834
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-11-26 00:21:32 +0100

    more

--- src/aoc22/day12.cpp
+++ src/aoc22/day12.cpp
@@ -92,7 +92,9 @@ veryfiInput(const Input& input)
       height) };
   }
 
-  if (const auto valueAtStart = grid[input.startPosition.y][input.startPosition.x]; valueAtStart != 'a') {
+  if (const auto valueAtStart =
+        grid[input.startPosition.y][input.startPosition.x];
+      valueAtStart != 'a') {
     throw Exception{ std::format("{} Input is ill formed : Value at {} start "
                                  "position ({},{}) should be 'a'",
                                  prefix,

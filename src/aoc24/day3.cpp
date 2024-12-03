f96f21f6a6aff1483100fde0dffacf4d54bd6da7
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-03 18:29:56 +0100

    better

--- src/aoc24/day3.cpp
+++ src/aoc24/day3.cpp
@@ -6,38 +6,35 @@
 
 namespace aoc24::day3 {
 Result
-solve(Input input) noexcept
+solve(Input input) noexcept // NOLINT(bugprone-exception-escape)
 {
   std::size_t part1 = 0;
   std::size_t part2 = 0;
-  try {
-    const std::string strInput{ input };
-    const std::regex regex(R"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))");
-    const auto endRegexIt = std::sregex_iterator();
-    auto toNumber = [](const auto& match) {
-      static constexpr int base = 10;
-      return std::strtoull(match.str().c_str(), nullptr, base); // NOLINT()
-    };
-    bool mulEnabled = true;
-    auto iter = std::sregex_iterator(strInput.begin(), strInput.end(), regex);
-    while (iter != endRegexIt) {
-      const auto matchedStr = iter->str();
-      if (matchedStr == "do()") {
-        mulEnabled = true;
-      } else if (matchedStr == "don't()") {
-        mulEnabled = false;
-      } else {
-        const auto left = toNumber((*iter)[1]);
-        const auto right = toNumber((*iter)[2]);
-        const auto mul = left * right;
-        part1 += mul;
-        if (mulEnabled) {
-          part2 += mul;
-        }
+  const std::string strInput{ input };
+  const std::regex regex(R"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))");
+  const auto endRegexIt = std::sregex_iterator();
+  auto toNumber = [](const auto& match) {
+    static constexpr int base = 10;
+    return std::strtoull(match.str().c_str(), nullptr, base); // NOLINT()
+  };
+  bool mulEnabled = true;
+  auto iter = std::sregex_iterator(strInput.begin(), strInput.end(), regex);
+  while (iter != endRegexIt) {
+    const auto matchedStr = iter->str();
+    if (matchedStr == "do()") {
+      mulEnabled = true;
+    } else if (matchedStr == "don't()") {
+      mulEnabled = false;
+    } else {
+      const auto left = toNumber((*iter)[1]);
+      const auto right = toNumber((*iter)[2]);
+      const auto mul = left * right;
+      part1 += mul;
+      if (mulEnabled) {
+        part2 += mul;
       }
-      ++iter;
     }
-  } catch (...) { // NOLINT
+    ++iter;
   }
   return { part1, part2 };
 }

14b2189170a659298ac56a46c03ab4fd5ce0f9fe
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-04 23:00:51 +0100

    first attempt

--- src/aoc24/day4.cpp
+++ src/aoc24/day4.cpp
@@ -1,13 +1,118 @@
 #include "aoc24/day4.hpp"
+#include <array>
+#include <cassert>
 #include <cstddef>
+#include <cstdint>
+#include <optional>
+#include <queue>
+#include <utility>
+
+namespace {
+
+enum class Direction : std::uint8_t
+{
+  W = 0,
+  E = 1,
+  S = 2,
+  N = 3,
+  NE = 4,
+  NW = 5,
+  SE = 6,
+  SW = 7,
+};
+
+struct Position
+{
+  std::size_t x;
+  std::size_t y;
+};
+
+using Neighbours = std::array<std::optional<Position>, 8>;
+using enum Direction;
+
+auto
+getNeighbours(const Position& position, const Position& bottomRight)
+{
+  Neighbours neighbours{};
+  const auto [x, y] = position;
+  const auto [w, h] = bottomRight;
+  if (x > 0) {
+    neighbours[static_cast<std::size_t>(W)] = { x - 1, y };
+  }
+  if (x < w - 1) {
+    neighbours[static_cast<std::size_t>(E)] = { x + 1, y };
+  }
+  if (y < h - 1) {
+    neighbours[static_cast<std::size_t>(S)] = { x, y + 1 };
+  }
+  if (y > 0) {
+    neighbours[static_cast<std::size_t>(N)] = { x, y - 1 };
+  }
+  if (x < w - 1 && y > 0) {
+    neighbours[static_cast<std::size_t>(NE)] = { x + 1, y - 1 };
+  }
+  if (x > 0 && y > 0) {
+    neighbours[static_cast<std::size_t>(NW)] = { x - 1, y - 1 };
+  }
+  if (x < w - 1 && y < h - 1) {
+    neighbours[static_cast<std::size_t>(SE)] = { x + 1, y + 1 };
+  }
+  if (x > 0 && y < h - 1) {
+    neighbours[static_cast<std::size_t>(SW)] = { x - 1, y + 1 };
+  }
+  return neighbours;
+}
+
+auto
+getNextLetter(aoc24::day4::Letter letter)
+{
+  return static_cast<aoc24::day4::Letter>(std::to_underlying(letter) + 1);
+}
+
+}
 
 namespace aoc24::day4 {
 
-  Result solve(const Input &input) noexcept {
-    const std::size_t part1 = input.size();
-    const std::size_t part2 = input.size();
+Result
+solve(const Input& input) noexcept
+{
+  const auto h = input.size();
+  const auto w = input[0].size();
+  std::size_t part1 = 0;
 
-    return {part1,part2};
+  auto getNeighbours = [&w, &h](const Position& position) {
+    return ::getNeighbours(position, Position{ w, h });
+  };
+
+  for (std::size_t y = 0; y < h; ++y) {
+    for (std::size_t x = 0; x < w; ++x) {
+      if ( input[y][x] != Letter::X) {
+        continue;
+      }
+      std::queue<Position> positionsToCheck{};
+      positionsToCheck.push({ x, y });
+      while (!positionsToCheck.empty()) {
+        const auto currentPosition = positionsToCheck.front();
+        auto  currentLetter = input[currentPosition.y][currentPosition.x];
+        const auto nextLetter = getNextLetter(currentLetter);
+        if (currentLetter == input[currentPosition.y][currentPosition.x]) {
+          if (currentLetter == Letter::S) {
+            ++part1;
+          } else {
+            for (const auto& neighbour : getNeighbours(currentPosition)) {
+              if (neighbour && input[neighbour->y][neighbour->x] == nextLetter ) {
+                positionsToCheck.push(*neighbour);
+              }
+            }
+          //  currentLetter = nextLetter;
+          }
+        }
+        positionsToCheck.pop();
+      }
+    }
   }
+
+  return { part1, part1 };
+}
 
 }
--- tests/aoc24/day4.cpp
+++ tests/aoc24/day4.cpp
@@ -1,15 +1,38 @@
 #include "day4.hpp"
 
-#include "aoc24/inputs.hpp.in"
+#include "inputs.hpp"
 #include "parsing.hpp"
 
+#include <algorithm>
 #include <catch2/catch_test_macros.hpp>
+#include <cstddef>
+#include <ranges>
 #include <string_view>
+#include <utility>
+#include <vector>
 
 namespace   {
   auto readInput(std::string_view path) {
       aoc24::day4::Input input{};
       inputs::FileReader fileReader{path};
+      using  enum aoc24::day4::Letter;
+      while (const auto& line = fileReader.readLine()) {
+          const auto letters = *line | std::views::transform([](const char c) {
+              switch(c) {
+                case 'X':
+                  return X;
+                case 'M':
+                  return M;
+                case 'A':
+                  return A;
+                case 'S':
+                  return S;
+                default:
+                  std::unreachable();
+              }
+          }) | std::ranges::to<std::vector>();
+          input.push_back(letters);       
+      }
       return input;
   }
 }
@@ -17,6 +40,13 @@ namespace   {
 TEST_CASE("Aoc24 Day4 Example", "[AoC24_Day4]")
 {
   const auto input = readInput(inputs::day4::EXAMPLE);
+  const auto [part1, _] = aoc24::day4::solve(input);
+  REQUIRE(part1 == 18);
+}
+
+TEST_CASE("Aoc24 Day4 Input", "[AoC24_Day4]")
+{
+  const auto input = readInput(inputs::day4::INPUT);
   const auto [part1, _] = aoc24::day4::solve(input);
   REQUIRE(part1 == 18);
 }

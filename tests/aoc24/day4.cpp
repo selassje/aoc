d922fd647102b83d97791571f3e324dfa41fe6ff
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-05 23:34:03 +0100

    correct formatting

--- tests/aoc24/day4.cpp
+++ tests/aoc24/day4.cpp
@@ -9,31 +9,32 @@
 #include <string_view>
 #include <utility>
 #include <vector>
-namespace   {
-  auto readInput(std::string_view path) {
-      aoc24::day4::Input input{};
-      inputs::FileReader fileReader{path};
-      using  enum aoc24::day4::Letter;
-      while (const auto& line = fileReader.readLine()) {
-          const auto letters = *line | std::views::transform([](const char c) {
-              switch(c) {
-                case 'X':
-                  return X;
-                case 'M':
-                  return M;
-                case 'A':
-                  return A;
-                case 'S':
-                  return S;
-                default:
-                  std::unreachable();
-              }
-
-          }) | std::ranges::to<std::vector>();
-          input.push_back(letters);       
+namespace {
+auto
+readInput(std::string_view path)
+{
+  aoc24::day4::Input input{};
+  inputs::FileReader fileReader{ path };
+  using enum aoc24::day4::Letter;
+  while (const auto& line = fileReader.readLine()) {
+    const auto letters = *line | std::views::transform([](const char c) {
+      switch (c) {
+        case 'X':
+          return X;
+        case 'M':
+          return M;
+        case 'A':
+          return A;
+        case 'S':
+          return S;
+        default:
+          std::unreachable();
       }
-      return input;
+    }) | std::ranges::to<std::vector>();
+    input.push_back(letters);
   }
+  return input;
+}
 }
 
 TEST_CASE("Aoc24 Day4 Example", "[AoC24_Day4]")

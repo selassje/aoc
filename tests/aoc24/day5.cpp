59026b6c764eae3d27b3849c6526509449a93569
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-05 21:48:14 +0100

    some parsing fixes

--- tests/aoc24/day5.cpp
+++ tests/aoc24/day5.cpp
@@ -18,15 +18,17 @@ namespace   {
       aoc24::day5::Updates updates{};
       inputs::FileReader fileReader{path};
       bool readOrders = true;
-      while (const auto& line = fileReader.readLine()) {
-          if ( line == "" ) {
+      while (auto line = fileReader.readLine()) {
+          if ( *line == "" ) {
               readOrders = false;
               continue;
           }
           if ( readOrders) {
+              std::replace(std::begin(*line), std::end(*line),'|',' ');
               const auto order = inputs::parseLine<std::size_t, std::size_t>(*line);
               orders.emplace_back(order);
           } else {
+              std::replace(std::begin(*line), std::end(*line),',',' ');
               const auto update = inputs::parseStringDynamic<std::size_t>(*line);
               updates.emplace_back(update);
           }      
@@ -37,7 +39,7 @@ namespace   {
 
 TEST_CASE("Aoc24 Day4 Example", "[AoC24_Day4]")
 {
-  const auto input = readInput(inputs::day4::EXAMPLE);
+  const auto input = readInput(inputs::day5::EXAMPLE);
   const auto [part1, _] = aoc24::day5::solve(input);
   REQUIRE(part1 == 143);
   //REQUIRE(part2 == 9);

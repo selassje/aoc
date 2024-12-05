e7527cb87f4fa2638c218fa155911bb92a4142d1
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-05 23:32:40 +0100

    part 2 clean

--- src/aoc24/day5.cpp
+++ src/aoc24/day5.cpp
@@ -1,21 +1,22 @@
 #include "aoc24/day5.hpp"
+#include <algorithm>
 #include <cstddef>
 #include <iterator>
+#include <utility>
+#include <vector>
 
 namespace aoc24::day5 {
 
 namespace {
 
+using Indexes = Update;
+
 bool
 arePagesInOrder(std::size_t page1, std::size_t page2, const Orders& orders)
 {
-  for (const auto& order : orders) {
-    if (order.first == page2 && order.second == page1) {
-      return false;
-    }
-  }
-
-  return true;
+  return std::ranges::all_of(orders, [&](const auto& order) {
+    return !(order.first == page2 && order.second == page1);
+  });
 }
 
 bool
@@ -46,10 +47,48 @@ isUpdateInOrder(const Update& update, const Orders& orders)
   return true;
 }
 
+Indexes
+getOutOfOrderPageIndexes(const Update& update, const Orders& orders)
+{
+  Indexes result{};
+  for (std::size_t i = 0; i < update.size(); ++i) {
+    if (!isPageInOrder(i, update, orders)) {
+      result.push_back(i);
+    }
+  }
+  return result;
+}
+
 std::size_t
 getMiddlePage(const Update& update)
 {
   return update[update.size() / 2];
+}
+
+Update
+correctUpdate(const Update& update, const Orders& orders)
+{
+  auto correctedUpdate = update;
+  const auto outOfOrderIndexes = getOutOfOrderPageIndexes(update, orders);
+  Update outOfOrderPages{};
+  for (const auto i : outOfOrderIndexes) {
+    outOfOrderPages.push_back(update[i]);
+    std::erase(correctedUpdate, update[i]);
+  }
+
+  for (const auto& page : outOfOrderPages) {
+    for (std::size_t i = 0; i <= correctedUpdate.size(); ++i) {
+      auto correctedCpy = correctedUpdate;
+      auto it = correctedCpy.begin();
+      std::advance(it, i);
+      correctedCpy.insert(it, page);
+      if (isUpdateInOrder(correctedCpy, orders)) {
+        std::swap(correctedCpy, correctedUpdate);
+        break;
+      }
+    }
+  }
+  return correctedUpdate;
 }
 
 }
@@ -58,13 +97,17 @@ Result
 solve(const Input& input)
 {
   std::size_t part1 = 0;
+  std::size_t part2 = 0;
   for (const auto& update : input.updates) {
     if (isUpdateInOrder(update, input.orders)) {
       part1 += getMiddlePage(update);
+    } else {
+      const auto correctedUpdate = correctUpdate(update, input.orders);
+      part2 += getMiddlePage(correctedUpdate);
     }
   }
 
-  return { part1, part1 };
+  return { part1, part2 };
 }
 
 }
--- tests/aoc24/day5.cpp
+++ tests/aoc24/day5.cpp
@@ -5,50 +5,51 @@
 #include "parsing.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+
+#include <algorithm>
 #include <cstddef>
-#include <ranges>
 #include <string_view>
-#include <utility>
 #include <vector>
 
-namespace   {
-  aoc24::day5::Input readInput(std::string_view path) {
-      aoc24::day5::Input input{};
-      aoc24::day5::Orders orders{};
-      aoc24::day5::Updates updates{};
-      inputs::FileReader fileReader{path};
-      bool readOrders = true;
-      while (auto line = fileReader.readLine()) {
-          if ( *line == "" ) {
-              readOrders = false;
-              continue;
-          }
-          if ( readOrders) {
-              std::replace(std::begin(*line), std::end(*line),'|',' ');
-              const auto order = inputs::parseLine<std::size_t, std::size_t>(*line);
-              orders.emplace_back(order);
-          } else {
-              std::replace(std::begin(*line), std::end(*line),',',' ');
-              const auto update = inputs::parseStringDynamic<std::size_t>(*line);
-              updates.emplace_back(update);
-          }      
-      }
-      return {orders,updates};
+namespace {
+aoc24::day5::Input
+readInput(std::string_view path)
+{
+  aoc24::day5::Orders orders{};
+  aoc24::day5::Updates updates{};
+  inputs::FileReader fileReader{ path };
+  bool readOrders = true;
+  while (auto line = fileReader.readLine()) {
+    if (line->empty()) {
+      readOrders = false;
+      continue;
+    }
+    if (readOrders) {
+      std::replace(std::begin(*line), std::end(*line), '|', ' ');
+      const auto order = inputs::parseLine<std::size_t, std::size_t>(*line);
+      orders.emplace_back(order);
+    } else {
+      std::replace(std::begin(*line), std::end(*line), ',', ' ');
+      const auto update = inputs::parseStringDynamic<std::size_t>(*line);
+      updates.emplace_back(update);
+    }
   }
+  return { orders, updates };
+}
 }
 
-TEST_CASE("Aoc24 Day4 Example", "[AoC24_Day4]")
+TEST_CASE("Aoc24 Day5 Example", "[AoC24_Day4]")
 {
   const auto input = readInput(inputs::day5::EXAMPLE);
-  const auto [part1, _] = aoc24::day5::solve(input);
+  const auto [part1, part2] = aoc24::day5::solve(input);
   REQUIRE(part1 == 143);
-  //REQUIRE(part2 == 9);
+  REQUIRE(part2 == 123);
 }
 
-TEST_CASE("Aoc24 Day4 Input", "[AoC24_Day4]")
+TEST_CASE("Aoc24 Day5 Input", "[AoC24_Day]")
 {
   const auto input = readInput(inputs::day5::INPUT);
-  const auto [part1, _] = aoc24::day5::solve(input);
+  const auto [part1, part2] = aoc24::day5::solve(input);
   REQUIRE(part1 == 5991);
-  //REQUIRE(part2 == 9);
+  REQUIRE(part2 == 5479);
 }

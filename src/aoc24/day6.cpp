45cf74e1a2dab7bcc4d9503449e788abb0175d69
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-06 11:04:00 +0100

    smth

--- src/aoc24/day6.cpp
+++ src/aoc24/day6.cpp
@@ -1,7 +1,41 @@
 #include "aoc24/day6.hpp"
 #include <cstddef>
+#include <cstdint>
+#include <optional>
 
 namespace aoc24::day6 {
+
+struct Position {
+  std::size_t x;
+  std::size_t y;
+};
+
+enum class Direction : std::uint8_t {
+    N,
+    E,
+    S,
+    W
+};
+
+struct GuardState {
+  Position position;
+  Direction direction;
+};
+
+namespace {
+
+  std::optional<GuardState> moveGuard(const GuardState& guard) {
+      switch (guard.direction) {
+          case Tile::
+
+      }
+      return std::nullopt;
+  }    
+
+
+
+}
+
 
 Result
 solve(const Input& input)

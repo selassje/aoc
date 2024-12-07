52297630347dea77aee2c7130dcca997961ae4a1
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-07 16:35:38 +0100

    example passing

--- src/aoc24/day7.cpp
+++ src/aoc24/day7.cpp
@@ -1,11 +1,15 @@
 #include "aoc24/day7.hpp"
+#include <algorithm>
 #include <cstddef>
+#include <limits>
+#include <print>
 #include <vector>
 
 namespace aoc24::day7 {
 
 namespace {
 
+constexpr std::size_t MAX = std::numeric_limits<std::size_t>::max();
 using Operands = std::vector<std::size_t>;
 
 bool
@@ -13,13 +17,23 @@ isEquationPossibleRec(std::size_t testValue,
                       std::size_t i,
                       const Operands& operands)
 {
+  bool add = true;
 
-  if (i == operands.size() - 1) {
-    return testValue == operands[i] || testValue / operands[i] == 1;
+  if (i == operands.size() - 2) {
+    std::size_t addV = 0;
+    if( MAX - operands[i] < operands[i+ 1]) {
+      addV = operands[i] + operands[i + 1];
+      add = true;
+    }
+    addV = operands[i] + operands[i + 1];
+    const auto multiplyV = operands[i] * operands[i + 1];
+
+    return ( add && testValue == addV) || testValue == multiplyV;
   }
-
-  const bool add =
-    isEquationPossibleRec(testValue - operands[i], i + 1, operands);
+  
+  if (testValue > operands[i]) {
+    add = isEquationPossibleRec(testValue - operands[i], i + 1, operands);
+  }
   const bool multiply =
     isEquationPossibleRec(testValue / operands[i], i + 1, operands);
   return add || multiply;
@@ -28,7 +42,7 @@ isEquationPossibleRec(std::size_t testValue,
 bool
 isEquationPossible(const Record& record)
 {
-  return isEquationPossibleRec(record.testValue, 1, record.operands);
+  return isEquationPossibleRec(record.testValue, 0, record.operands);
 }
 
 }
@@ -36,9 +50,14 @@ isEquationPossible(const Record& record)
 Result
 solve(const Input& input)
 {
+
+
   std::size_t part1 = 0;
   for (const auto& record : input) {
-    if (isEquationPossible(record)) {
+    auto reversedRecord = record;
+    std::reverse(reversedRecord.operands.begin(),
+                 reversedRecord.operands.end());
+    if (isEquationPossible(reversedRecord)) {
       part1 += record.testValue;
     }
   }

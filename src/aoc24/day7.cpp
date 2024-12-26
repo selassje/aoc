4e1f5ecd6fc7b4cf8bfa66de6594e3b9851fe57c
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-26 21:47:22 +0100

    part1 raw

--- src/aoc24/day7.cpp
+++ src/aoc24/day7.cpp
@@ -1,5 +1,6 @@
 #include "aoc24/day7.hpp"
 #include <algorithm>
+#include <cmath>
 #include <cstddef>
 #include <limits>
 #include <print>
@@ -8,7 +9,9 @@
 namespace aoc24::day7 {
 
 namespace {
-
+// 5512534504936
+// 12343796585840
+// 18446744073709551615
 constexpr std::size_t MAX = std::numeric_limits<std::size_t>::max();
 using Operands = std::vector<std::size_t>;
 
@@ -17,25 +20,32 @@ isEquationPossibleRec(std::size_t testValue,
                       std::size_t i,
                       const Operands& operands)
 {
-  bool add = true;
+  bool add = false;
+  bool multiply = false;
 
   if (i == operands.size() - 2) {
     std::size_t addV = 0;
-    if( MAX - operands[i] < operands[i+ 1]) {
+    std::size_t multiplyV = 0;
+    if (MAX - operands[i] > operands[i + 1]) {
       addV = operands[i] + operands[i + 1];
       add = true;
     }
-    addV = operands[i] + operands[i + 1];
-    const auto multiplyV = operands[i] * operands[i + 1];
+    if (MAX / operands[i] > operands[i + 1]) {
+      multiplyV = operands[i] * operands[i + 1];
+      multiply = true;
+    }
+    //    addV = operands[i] + operands[i + 1];
+    // const auto multiplyV = operands[i] * operands[i + 1];
 
-    return ( add && testValue == addV) || testValue == multiplyV;
+    return (add && testValue == addV) || (multiply && testValue == multiplyV);
   }
-  
+
   if (testValue > operands[i]) {
     add = isEquationPossibleRec(testValue - operands[i], i + 1, operands);
   }
-  const bool multiply =
-    isEquationPossibleRec(testValue / operands[i], i + 1, operands);
+  if (testValue > operands[i] && testValue % operands[i] == 0) {
+    multiply = isEquationPossibleRec(testValue / operands[i], i + 1, operands);
+  }
   return add || multiply;
 }
 
@@ -45,19 +55,50 @@ isEquationPossible(const Record& record)
   return isEquationPossibleRec(record.testValue, 0, record.operands);
 }
 
+bool
+isEquationPossible2(const Record& record)
+{
+  const std::size_t operatorsCount = record.operands.size() - 1;
+  const std::size_t combinationsCount = std::size_t{ 1 } << operatorsCount;
+  for (std::size_t combination = 0; combination < combinationsCount;
+       ++combination) {
+    std::size_t result = 0;
+    for (std::size_t i = 0; i < operatorsCount; ++i) {
+      const bool isMultiply = (combination & (std::size_t{1} << i)) != 0;
+      if( i == 0) {
+        if ( isMultiply) {
+          result = record.operands[0] * record.operands[1];
+        } else {
+          result = record.operands[0] + record.operands[1];
+        }
+      }
+      else {
+        if ( isMultiply) {
+          result = result * record.operands[i + 1];
+        } else {
+          result = result + record.operands[i + 1];
+        }
+      }
+    }
+    if (result == record.testValue) {
+      return true;
+    }
+  }
+  return false;
+}
+
 }
 
 Result
 solve(const Input& input)
 {
 
-
   std::size_t part1 = 0;
   for (const auto& record : input) {
     auto reversedRecord = record;
     std::reverse(reversedRecord.operands.begin(),
                  reversedRecord.operands.end());
-    if (isEquationPossible(reversedRecord)) {
+    if (isEquationPossible2(record)) {
       part1 += record.testValue;
     }
   }

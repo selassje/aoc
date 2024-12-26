e13d08052b3c13fc7b9ce0fcbe33f6040b915d95
Przemyslaw Koziol <przemkoz85@gmail.com>
2024-12-26 22:22:46 +0100

    more

--- src/aoc24/day7.cpp
+++ src/aoc24/day7.cpp
@@ -13,37 +13,6 @@ namespace aoc24::day7 {
 namespace {
 using Operands = std::vector<std::size_t>;
 
-bool
-isEquationPossible2(const Record& record)
-{
-  const std::size_t operatorsCount = record.operands.size() - 1;
-  const std::size_t combinationsCount = std::size_t{ 1 } << operatorsCount;
-  for (std::size_t combination = 0; combination < combinationsCount;
-       ++combination) {
-    std::size_t result = 0;
-    for (std::size_t i = 0; i < operatorsCount; ++i) {
-      const bool isMultiply = (combination & (std::size_t{ 1 } << i)) != 0;
-      if (i == 0) {
-        if (isMultiply) {
-          result = record.operands[0] * record.operands[1];
-        } else {
-          result = record.operands[0] + record.operands[1];
-        }
-      } else {
-        if (isMultiply) {
-          result = result * record.operands[i + 1];
-        } else {
-          result = result + record.operands[i + 1];
-        }
-      }
-    }
-    if (result == record.testValue) {
-      return true;
-    }
-  }
-  return false;
-}
-
 std::size_t
 concatenate(std::size_t a, std::size_t b)
 {
@@ -53,18 +22,20 @@ concatenate(std::size_t a, std::size_t b)
   return std::strtoull(concStr.c_str(), nullptr, 10); // NOLINT
 }
 
+template<int N>
 bool
-isEquationPossible3(const Record& record)
+isEquationPossible(const Record& record)
 {
   const std::size_t operatorsCount = record.operands.size() - 1;
-  const auto combinationsCount = static_cast<std::size_t>(
-    std::pow(4.0, static_cast<double>(operatorsCount)));
+  const std::size_t combinationsCount = std::size_t{ 1 }
+                                        << operatorsCount * (N / 2);
 
   for (std::size_t combination = 0; combination < combinationsCount;
        ++combination) {
     std::size_t result = 0;
     for (std::size_t i = 0; i < operatorsCount; ++i) {
-      const auto opType = (combination & (std::size_t{ 3 } << 2 * i)) >> 2 * i;
+      const auto opType =
+        (combination & (std::size_t{ N - 1 } << (N / 2) * i)) >> (N / 2) * i;
       if (i == 0) {
         if (opType == 0) {
           result = record.operands[0] * record.operands[1];
@@ -102,10 +73,10 @@ solve(const Input& input)
   std::size_t part1 = 0;
   std::size_t part2 = 0;
   for (const auto& record : input) {
-    if (isEquationPossible2(record)) {
+    if (isEquationPossible<2>(record)) {
       part1 += record.testValue;
     }
-    if (isEquationPossible3(record)) {
+    if (isEquationPossible<4>(record)) {
       part2 += record.testValue;
     }
   }

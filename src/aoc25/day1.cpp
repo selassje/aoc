7e6bbd241527f8510834d5f56491e333129df51c
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-01 21:23:02 +0100

    bettter

--- src/aoc25/day1.cpp
+++ src/aoc25/day1.cpp
@@ -2,91 +2,71 @@
 #include "aoc25/day1.hpp"
 #include <atomic>
 #include <cstdint>
+#include <iostream>
 #include <iterator>
 #include <limits>
-#include <iostream>
 
 namespace {
 
-std::uint64_t rotate(std::uint64_t position, const aoc25::day1::Rotation &rotation) {
-  constexpr auto maxCount = std::numeric_limits<std::uint64_t>::max();
-  
-  if ( rotation.direction == aoc25::day1::Direction::Right){
-    //  std::cout <<"Ooops\n";
-    if (rotation.count > maxCount - position) {
-      std::cout <<"Ooops\n";
-    }
-    
-    position = (position + rotation.count) % 100; //NOLINT
-  } else {
-    if (position >= rotation.count) {
-      position -= rotation.count;
-    }
-    else {
-      //position = 100 - ((rotation.count - position) % 100); //NOLINT
-      auto count  = rotation.count;
-      count -= position; 
-      position = 0;
-      position = (100 - ((count - position) % 100)) % 100; //NOLINT
-    }
+struct RotationResult
+{
+  std::uint64_t position;
+  std::uint64_t timesOPassed;
+};
 
-  }
-  return position;
-}
-std::uint64_t rotate2(std::uint64_t position, const aoc25::day1::Rotation &rotation) {
-  constexpr auto maxCount = std::numeric_limits<std::uint64_t>::max();
-  
-  if ( rotation.direction == aoc25::day1::Direction::Right){
-    //  std::cout <<"Ooops\n";
-    if (rotation.count > maxCount - position) {
-      std::cout <<"Ooops\n";
-    }
-    return  (position + rotation.count) / 100; //NOLINT
+RotationResult
+rotate(std::uint64_t position, const aoc25::day1::Rotation& rotation)
+{
+  constexpr auto maxCount = 100;
+  std::uint64_t times0Passed = 0;
+
+  if (rotation.direction == aoc25::day1::Direction::Right) {
+    const auto rawShifted = position + rotation.count;
+    position = rawShifted % maxCount;
+    times0Passed = rawShifted / maxCount;
   } else {
     if (position >= rotation.count) {
       position -= rotation.count;
-      if(position == 0) {
-        return 1;
+      if (position == 0) {
+        times0Passed = 1;
       }
-    }
-    else {
-      auto count  = rotation.count;
-      //position = 100 - ((rotation.count - position) % 100); //NOLINT
-      if ( position == 0) {
-        return count/100;
-      }
+    } else {
+      auto count = rotation.count;
+      const auto origPosition = position;
 
       count -= position;
-      std::uint64_t result = 1; 
+      std::uint64_t result = 1;
       position = 0;
-      position = (100 - ((count - position) % 100)) % 100; //NOLINT
-      return (count/100) + 1;    //NOLINT
+      position = (100 - ((count - position) % 100)) % 100; // NOLINT
+      if (origPosition == 0) {
+         times0Passed = (rotation.count / maxCount);
+      }
+      else {
+         times0Passed = (count / maxCount) + 1;
+      }
     }
-
   }
-  return 0;
+  return { position, times0Passed };
 }
 
 }
-
 
 namespace aoc25::day1 {
 Result
-solve(const Input &input)
+solve(const Input& input)
 {
-  std::uint64_t position = 50; //NOLINT
+  std::uint64_t position = 50; // NOLINT
   std::uint64_t part1 = 0;
   std::uint64_t part2 = 0;
 
-  for ( const auto& rotation : input) {
-      const auto positionCpy = position;
-      part2 += rotate2(positionCpy, rotation);
-      position = rotate(position, rotation);
-      if ( position == 0)
-      {
-        ++part1;
-      }
+  for (const auto& rotation : input) {
+    const auto rotationResult = rotate(position, rotation);
+    position = rotationResult.position;
+    if (position == 0) {
+      ++part1;
+    }
+    part2 += rotationResult.timesOPassed;
   }
-  return {part1, part2};
+  return { part1, part2 };
 }
 }

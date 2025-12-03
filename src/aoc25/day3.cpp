cfa99bbf8a6d718ebd9c6759cb91036bfbe5f4e6
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-03 10:21:31 +0100

    more

--- src/aoc25/day3.cpp
+++ src/aoc25/day3.cpp
@@ -1,5 +1,18 @@
 #include "aoc25/day3.hpp"
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
 
+namespace {
+using aoc25::day3::Bank;
+std::uint64_t
+getMaxJoltage(const Bank& bank)
+{
+  auto sortedBank = bank;
+  std::ranges::sort((sortedBank));
+  return static_cast<std::uint64_t>(sortedBank[0] * sortedBank[1]);
+}
+}
 
 namespace aoc25::day3 {
 Result
@@ -7,6 +20,10 @@ solve(const Input& input)
 {
   std::uint64_t part1 = 0;
   std::uint64_t part2 = 0;
+  for (const auto& bank : input) {
+    part1 += getMaxJoltage(bank);
+  }
+  part2 = part1;
   return { part1, part2 };
 }
 }
--- tests/aoc25/day3.cpp
+++ tests/aoc25/day3.cpp
@@ -19,13 +19,9 @@ readInput(std::string_view path)
 {
   Input input{};
   FileReader fileReader{ path };
-  if (auto line = fileReader.readLine()) {
-    std::replace(line->begin(), line->end(), '-', ' ');
-    std::replace(line->begin(), line->end(), ',', ' ');
-    const auto numbers = inputs::parseStringDynamic<std::uint64_t>(*line);
-    for (std::size_t i = 0; i < numbers.size(); i += 2) {
-      input.emplace_back(numbers[i], numbers[i + 1]);
-    }
+  while (const auto& line = fileReader.readLine()) {
+    const auto numbers = inputs::parseStringDynamic<std::uint8_t>(*line);
+    input.emplace_back(numbers); 
   }
   return input;
 }
@@ -35,8 +31,8 @@ TEST_CASE("Aoc25 Day3 Example", "[AoC25_Day3]")
 {
   const auto input = readInput(inputs::day3::EXAMPLE);
   const auto [part1, part2] = aoc25::day3::solve(input);
-  REQUIRE(part1 == 1227775554);
-  REQUIRE(part2 == 4174379265);
+  REQUIRE(part1 == 357);
+  REQUIRE(part2 == 357);
 }
 
 TEST_CASE("Aoc25 Day3 Input", "[AoC25_Day3]")

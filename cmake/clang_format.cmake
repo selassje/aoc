d3442e22fbf8354e36f8373ac0a50cd04ea715eb
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-08 00:06:55 +0100

    simplify clang-format  input paths

--- cmake/clang_format.cmake
+++ cmake/clang_format.cmake
@@ -1,22 +1,13 @@
 file(
   GLOB
   ALL_CXX_SOURCE_FILES
-  ${CMAKE_SOURCE_DIR}/src/aoc22/*.cpp
-  ${CMAKE_SOURCE_DIR}/src/aoc23/*.cpp
-  ${CMAKE_SOURCE_DIR}/src/aoc24/*.cpp
-  ${CMAKE_SOURCE_DIR}/src/aoc25/*.cpp
-  ${CMAKE_SOURCE_DIR}/include/aoc22/*.hpp
-  ${CMAKE_SOURCE_DIR}/include/aoc23/*.hpp
-  ${CMAKE_SOURCE_DIR}/include/aoc24/*.hpp
-  ${CMAKE_SOURCE_DIR}/include/aoc25/*.hpp
-  ${CMAKE_SOURCE_DIR}/import/aoc25/*.cppm
+  ${CMAKE_SOURCE_DIR}/src/aoc*/*
+  ${CMAKE_SOURCE_DIR}/include/aoc*/*.hpp
+  ${CMAKE_SOURCE_DIR}/import/aoc*/*.cppm
   ${CMAKE_SOURCE_DIR}/src/common/*
   ${CMAKE_SOURCE_DIR}/tests/*.hpp
   ${CMAKE_SOURCE_DIR}/tests/*.cpp
-  ${CMAKE_SOURCE_DIR}/tests/aoc22/*.cpp
-  ${CMAKE_SOURCE_DIR}/tests/aoc23/*.cpp
-  ${CMAKE_SOURCE_DIR}/tests/aoc24/*.cpp
-  ${CMAKE_SOURCE_DIR}/tests/aoc25/*.cpp)
+  ${CMAKE_SOURCE_DIR}/tests/aoc*/*.cpp)
 
 if("${ALL_CXX_SOURCE_FILES}" STREQUAL "")
   message(SEND_ERROR "No files to format")

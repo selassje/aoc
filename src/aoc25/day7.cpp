731575afb8239a9f3c43fca37e0c08a97e551fe9
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-07 21:25:17 +0100

    cleaner

--- src/aoc25/day7.cpp
+++ src/aoc25/day7.cpp
@@ -2,9 +2,7 @@ module;
 
 #include <cstddef>
 #include <cstdint>
-#include <flat_map>
-#include <print>
-#include <set>
+#include <optional>
 #include <vector>
 
 module aoc25.day7;
@@ -30,16 +28,13 @@ struct Point
   }
 };
 
-using Path = std::vector<Point>;
-using UniquePaths = std::set<Path>;
-using PositionToSplitterMap = std::flat_map<Point, std::size_t>;
-
 using Matrix = aoc::matrix::Matrix<TileEx>;
 
 namespace {
-bool
-tryUpdate(Matrix& matrix, std::uint64_t& splittedCount)
+auto
+tryUpdate(Matrix& matrix)
 {
+  std::uint64_t splittedCount = 0;
   bool updated = false;
   for (std::size_t y = 0; y < matrix.height(); ++y) {
     for (std::size_t x = 0; x < matrix.width(); ++x) {
@@ -67,9 +62,51 @@ tryUpdate(Matrix& matrix, std::uint64_t& splittedCount)
       }
     }
   }
-  return updated;
+  return updated ? std::optional<std::uint64_t>{ splittedCount } : std::nullopt;
 }
 
+auto
+countSplits(const Matrix& matrix)
+{
+  Matrix tmpMatrix = matrix;
+  const auto start = tmpMatrix.find(TileEx::Start)[0];
+  tmpMatrix[start.x, start.y] = TileEx::Beam;
+  std::uint64_t count = 0;
+  while (const auto splitted = tryUpdate(tmpMatrix)) {
+    count += *splitted;
+  }
+  return count;
+}
+
+auto
+countUniqueTimelines(const Matrix& matrix)
+{
+  const auto start = matrix.find(TileEx::Start)[0];
+  aoc::matrix::Matrix dp{ { matrix.width(), matrix.height() }, 0ULL };
+  for (std::size_t x = 0; x < matrix.width(); ++x) {
+    dp[x, matrix.height() - 1] = 1;
+  }
+
+  for (std::size_t i = 1; i < matrix.height(); ++i) {
+    std::size_t y = matrix.height() - 1 - i;
+    for (std::size_t x = 0; x < matrix.width(); ++x) {
+      if (matrix[x, y] == TileEx::Empty || matrix[x, y] == TileEx::Start) {
+
+        if (matrix[x, y + 1] != TileEx::Splitter) {
+          dp[x, y] += dp[x, y + 1];
+        } else {
+          if (x > 0) {
+            dp[x, y] += dp[x - 1, y + 1];
+          }
+          if (x < matrix.width() - 1) {
+            dp[x, y] += dp[x + 1, y + 1];
+          }
+        }
+      }
+    }
+  }
+  return dp[start.x, start.y]; //
+}
 }
 
 namespace aoc25::day7 {
@@ -77,44 +114,8 @@ Result
 solve(const Input& input)
 {
   Matrix matrix(input, [](Tile t) { return static_cast<TileEx>(t); });
-  Matrix originalMatrix = matrix;
-  const auto start = matrix.find(TileEx::Start)[0];
-  matrix[start.x, start.y] = TileEx::Beam;
-
-  std::uint64_t part1 = 0;
-  while (tryUpdate(matrix, part1)) {
-    // keep updating
-  }
- aoc::matrix::Matrix dp{{ originalMatrix.width(), originalMatrix.height() }, 0ULL };
-
-  for(std::size_t x = 0; x < matrix.width(); ++x) {
-      dp[x, matrix.height() - 1] = 1;
-  }
-
-  for (std::size_t i = 1; i < matrix.height(); ++i) {
-    std::size_t y = matrix.height() - 1 - i;
-    for (std::size_t x = 0; x < matrix.width(); ++x) {
-      if (originalMatrix[x, y] == TileEx::Empty ||  originalMatrix[x, y] == TileEx::Start) {
-          //originalMatrix[x, y] == TileEx::Splitter) 
-        
-        
-        if(originalMatrix[x, y + 1] != TileEx::Splitter) {
-          dp[x, y] += dp[x, y + 1];
-        }
-        else {
-          if(x > 0) {
-            dp[x, y] += dp[x - 1, y + 1];
-          }
-          if(x < matrix.width() - 1) {
-            dp[x, y] += dp[x + 1, y + 1];
-          }
-        }  
-
-      }
-    }
-  }
-  const std::uint64_t part2 = dp[start.x, start.y];//
- // part2 = dp[start.x, matrix.height() - 1];
+  const std::uint64_t part1 = countSplits(matrix);
+  const std::uint64_t part2 = countUniqueTimelines(matrix);
   return { part1, part2 };
 }
 }

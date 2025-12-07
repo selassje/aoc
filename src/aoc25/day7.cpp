ce2d12461a565f971a8b61a9aff4642a9c29e0a2
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-07 19:56:36 +0100

    test

--- src/aoc25/day7.cpp
+++ src/aoc25/day7.cpp
@@ -2,6 +2,9 @@ module;
 
 #include <cstddef>
 #include <cstdint>
+#include <flat_map>
+#include <print>
+#include <set>
 #include <vector>
 
 module aoc25.day7;
@@ -15,6 +18,21 @@ enum class TileEx : std::uint8_t
   Splitter,
   Beam,
 };
+
+struct Point
+{
+  std::size_t x;
+  std::size_t y;
+
+  bool operator<(const Point& other) const
+  {
+    return (y < other.y) || (y == other.y && x < other.x);
+  }
+};
+
+using Path = std::vector<Point>;
+using UniquePaths = std::set<Path>;
+using PositionToSplitterMap = std::flat_map<Point, std::size_t>;
 
 using Matrix = aoc::matrix::Matrix<TileEx>;
 
@@ -52,6 +70,57 @@ tryUpdate(Matrix& matrix, std::uint64_t& splittedCount)
   return updated;
 }
 
+std::optional<Path>
+trySplitterCombination(const Matrix& matrix,
+                       std::uint64_t combination,
+                       Point startPosition,
+                       const PositionToSplitterMap& splitterMap)
+{
+  Matrix tempMatrix = matrix;
+  Path path{ startPosition };
+  Point currentPosition = startPosition;
+  while (currentPosition.y < tempMatrix.height() - 1) {
+    if (tempMatrix[currentPosition.x, currentPosition.y + 1] == TileEx::Empty) {
+      tempMatrix[currentPosition.x, currentPosition.y + 1] = TileEx::Beam;
+      currentPosition.y += 1;
+      path.push_back(currentPosition);
+    } else if (tempMatrix[currentPosition.x, currentPosition.y + 1] ==
+               TileEx::Splitter) {
+      Point splitterPoint{ currentPosition.x, currentPosition.y + 1 };
+      const auto splitterIndex = splitterMap.find(splitterPoint)->second;
+      const bool goLeft = (combination & (1ULL << splitterIndex)) == 0;
+      if (goLeft) {
+        if (currentPosition.x > 0 &&
+            tempMatrix[currentPosition.x - 1, currentPosition.y + 1] ==
+              TileEx::Empty) {
+          tempMatrix[currentPosition.x - 1, currentPosition.y + 1] =
+            TileEx::Beam;
+          currentPosition.x -= 1;
+          currentPosition.y += 1;
+          path.push_back(currentPosition);
+        } else {
+          return std::nullopt;
+        }
+      } else {
+        if (currentPosition.x < tempMatrix.width() - 1 &&
+            tempMatrix[currentPosition.x + 1, currentPosition.y + 1] ==
+              TileEx::Empty) {
+          tempMatrix[currentPosition.x + 1, currentPosition.y + 1] =
+            TileEx::Beam;
+          currentPosition.x += 1;
+          currentPosition.y += 1;
+          path.push_back(currentPosition);
+        } else {
+          return std::nullopt;
+        }
+      }
+
+    } else {
+      return std::nullopt;
+    }
+  }
+  return path;
+}
 }
 
 namespace aoc25::day7 {
@@ -59,6 +128,7 @@ Result
 solve(const Input& input)
 {
   Matrix matrix(input, [](Tile t) { return static_cast<TileEx>(t); });
+  Matrix originalMatrix = matrix;
   const auto start = matrix.find(TileEx::Start)[0];
   matrix[start.x, start.y] = TileEx::Beam;
 
@@ -66,25 +136,44 @@ solve(const Input& input)
   while (tryUpdate(matrix, part1)) {
     // keep updating
   }
+  /*
+    auto print = [](TileEx t) {
+      switch (t) {
+        case TileEx::Empty:
+          return '.';
+        case TileEx::Start:
+          return 'S';
+        case TileEx::Splitter:
+          return '^';
+        case TileEx::Beam:
+          return '|';
+        default:
+          return '?';
+      }
+    };
+  */
+  // matrix.print(print);
+  const auto splitters = matrix.find(TileEx::Splitter);
+  const auto combinationsCount = 1ULL << splitters.size();
+  PositionToSplitterMap splitterMap;
+  UniquePaths uniquePaths;
+  for (std::size_t i = 0; i < splitters.size(); ++i) {
+    Point point = { splitters[i].x, splitters[i].y };
+    splitterMap[point] = i;
+  }
 
-  auto print = [](TileEx t) {
-    switch (t) {
-      case TileEx::Empty:
-        return '.';
-      case TileEx::Start:
-        return 'S';
-      case TileEx::Splitter:
-        return '^';
-      case TileEx::Beam:
-        return '|';
-      default:
-        return '?';
+  std::uint64_t part2 = 0;
+  for (std::uint64_t combination = 0; combination < combinationsCount;
+       ++combination) {
+    const auto pathOpt = trySplitterCombination(
+      originalMatrix, combination, Point{ start.x, start.y }, splitterMap);
+    if (pathOpt.has_value()) {
+      //std::print("Inserting\n");
+      //++part2;
+      uniquePaths.insert(*pathOpt);
     }
-  };
-
-  //matrix.print(print);
-
-  std::uint64_t part2 = part1;
+  }
+  part2 = uniquePaths.size();
   return { part1, part2 };
 }
 }

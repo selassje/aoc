3219c9f36bea89ae9e538437bf11bd3f2dac21a4
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-07 09:57:52 +0100

    formatting

--- tests/aoc25/day7.cpp
+++ tests/aoc25/day7.cpp
@@ -4,6 +4,6 @@ import aoc25.day7;
 
 TEST_CASE("Aoc25 Day6 Example", "[AoC25_Day6]")
 {
-  const auto part1 = aoc25::day7::solve(); //NOLINT
+  const auto part1 = aoc25::day7::solve(); // NOLINT
   REQUIRE(part1 == true);
 }

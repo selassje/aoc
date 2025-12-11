9251467e5154a9b7a61c5a98f9900d62f90a0510
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-11 20:08:03 +0100

    more

--- src/aoc25/day10.cpp
+++ src/aoc25/day10.cpp
@@ -2,11 +2,29 @@ module aoc25.day10;
 
 import std;
 
+namespace {
+
+using namespace aoc25::day10;
+
+std::uint64_t
+countMinimumPresses(const Machine& machine)
+{
+  // Placeholder implementation
+  return 0;
+}
+}
+
 namespace aoc25::day10 {
 
-  Result solve(const Input& input) {
-    std::uint64_t part1  = 0;
-    std::uint64_t part2  = 0;
-    return {part1, part2}; 
+Result
+solve(const Input& input)
+{
+  std::uint64_t part1 = 0;
+  std::uint64_t part2 = 0;
+  for (const auto& machine : input) {
+    part1 += countMinimumPresses(machine);
   }
+  part2 =  part1;
+  return { part1, part2 };
+}
 }

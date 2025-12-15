cae84f9b718585f6b28667a49945eac07dedc1ab
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-16 00:30:45 +0100

    more

--- src/aoc25/day12.cpp
+++ src/aoc25/day12.cpp
@@ -1,12 +1,22 @@
 module aoc25.day12;
 
 import std;
+import aoc.matrix;
 
+using Matrix = aoc::matrix::Matrix<aoc25::day12::Tile>;
 namespace aoc25::day12 {
 
 Result
 solve(const Input& input)
 {
+  auto shapes = std::vector<Matrix>{};
+  for (std::size_t i = 0; i < input.shapes.size(); ++i) {
+    shapes.emplace_back(input.shapes[i].grid, std::identity{});
+    shapes.back().print([](auto tile) { return tile == Tile::Full ? '#' : '.'; });
+    std::println();
+  }
+  const auto regions = input.regions;
+
   return { 0, 0 };
 }
 }

7177b3fa154ee9f77d93e9a45d31052502355cc3
Przemyslaw Koziol <przemkoz85@gmail.com>
2025-12-16 00:13:05 +0100

    prep

--- tests/aoc25/day12_test.cpp
+++ tests/aoc25/day12_test.cpp
@@ -7,13 +7,53 @@ import aoc25.day12;
 import std;
 
 using aoc25::day12::Input;
+using aoc25::day12::Region;
+using aoc25::day12::Shape;
 namespace {
 auto
 readInput(std::string_view path)
 {
   Input input{};
   auto fileReader = inputs::FileReader{ path };
-  while (const auto& line = fileReader.readLine()) {
+  Shape shape{};
+  bool readingShape = true;
+  while (auto line = fileReader.readLine()) {
+    if (!line->empty()) {
+      if (line->contains(':')) {
+        if (line->contains('x')) {
+          readingShape = false;
+          Region region{};
+          std::ranges::replace(*line, ':', ' ');
+          std::ranges::replace(*line, 'x', ' ');
+          const auto numbers = inputs::parseStringDynamic<std::uint64_t>(*line);
+          region.width = numbers[0];
+          region.height = numbers[1];
+          region.shapesCount =
+            std::vector<std::uint64_t>(numbers.begin() + 2, numbers.end());
+          input.regions.push_back(region);
+
+        } else {
+          std::ranges::replace(*line, ':', ' ');
+          const auto [id] = inputs::parseLine<std::uint64_t>(*line);
+          shape.id = id;
+          shape.grid.clear();
+          continue;
+        }
+      }
+      if (readingShape) {
+        auto tiles =
+          std::views::transform(*line,
+                                [](char c) {
+                                  return c == '#' ? aoc25::day12::Tile::Full
+                                                  : aoc25::day12::Tile::Empty;
+                                }) |
+          std::ranges::to<std::vector>();
+        shape.grid.push_back(tiles);
+      }
+
+    } else {
+      input.shapes.push_back(shape);
+    }
   }
   return input;
 }
@@ -26,7 +66,6 @@ TEST_CASE("Aoc25 Day12 Example", "[AoC25_Day12]")
   REQUIRE(part1 == 2);
   REQUIRE(part2 == 2);
 }
-
 
 TEST_CASE("Aoc25 Day12 Input", "[AoC25_Day12]")
 {
